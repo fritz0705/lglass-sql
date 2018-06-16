0bbb26b0328ef74c180bb8fa347ca9d4eea17247
Fritz Grimpen <fritz@grimpen.net>
2018-06-16 23:39:43 +0000

    autopep8

--- lglass_sql/ipam.py
+++ lglass_sql/ipam.py
@@ -4,8 +4,9 @@ import lglass.ipam
 
 import lglass_sql.nic
 
+
 class IPAMDatabase(lglass_sql.nic.NicDatabase,
-        lglass.ipam.IPAMDatabaseMixin):
+                   lglass.ipam.IPAMDatabaseMixin):
     def __init__(self, *args, **kwargs):
         lglass_sql.nic.NicDatabase.__init__(self, *args, **kwargs)
         lglass.ipam.IPAMDatabaseMixin.__init__(self)
